4d134cfff784fc71ace8db64f4707bb9572dff8b
Jack Oats <jackoatmeals@gmail.com>
2023-05-25 18:56:43 -0500

    font causing app to crash. will continue with project with NextJS

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,26 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   rollupOptions: {
-  //     output: {
-  //       assetFileNames: 'assets/pixelText.ttf',
-  //     },
-  //   },
-  // },
+  build: {
+    rollupOptions: {
+      output: {
+        // assetFileNames: 'fonts/pixelText.ttf',
+      },
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/assets/styles/main.scss";`
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': 'src',
+    }
+  }
 })
